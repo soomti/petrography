aad0ee736213e128bf79bb62f737686c3156437e
숨팅 <soom93@gmail.com>
2019-01-29 23:55:50 +0900

    Revert "Feature/add template"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree ../../../vendor/assets/javascripts/.
 //= require turbolinks
 //= require_tree .
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,3 @@
 class Board < ActiveRecord::Base
-  belongs_to :user
+  belong-to :user
 end

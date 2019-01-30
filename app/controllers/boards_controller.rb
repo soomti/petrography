d634fcba70c53cbc553de09d3cb81a5b159f3975
soomti <soom93@gmail.com>
2019-01-31 00:49:13 +0900

    board user auth 추가

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,6 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index]
   # GET /boards
   # GET /boards.json
   def index

828aded23bf9da67291fc48f59611c5ab9df07c5
soomti <soom93@gmail.com>
2019-01-31 00:50:39 +0900

    디바이스 컨트롤러로 이동할 수 있게 설정 변경

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :boards
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'user/registrations', sessions: 'user/sessions', passwords: 'user/passwords' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

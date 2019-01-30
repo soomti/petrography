373a8ed47cdc86848efd93b74a0801c24fa802ff
jmini1234 <shjj09@naver.com>
2019-01-30 18:45:25 +0900

    image_size_limit

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,13 @@
 class Board < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  validates_processing_of :image
+  validate :image_size_validation
+ 
+private
+  def image_size_validation
+    errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+  end
+  
 end

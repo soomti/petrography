e71d566515e24f87b2f1a0a0fdddafc58e0f8250
= <=>
2019-02-02 02:26:40 +0900

    board helper space line modify

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -2,12 +2,12 @@ module BoardsHelper
   extend self
   def get_category
     grouped_options = {
-        'category' => [
-            ['cat','cat'],
-            ['dog','dog'],
-            ['etc','etc']
-        ]
+      'category' => [
+        ['cat','cat'],
+        ['dog','dog'],
+        ['etc','etc']
+      ]
     }
     return grouped_options
-end
+  end
 end

b20545657b3f7280182a5b59ed3c84bc15c28f7f
hataerin <goil1113@likelion.org>
2019-02-02 18:03:43 +0900

    이메일 인증 관련 컬럼 추가

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190128111010) do
+ActiveRecord::Schema.define(version: 20190201104614) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "image"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20190128111010) do
     t.string   "url"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

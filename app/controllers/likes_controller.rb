3472f1cf41372ca01472a92e86bc00817257821c
cho <wjcho0511@naver.com>
2019-02-06 16:09:11 +0900

    likes_controller ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,14 @@
 class LikesController < ApplicationController
   def like_toggle
-    like = Like.find_by(user_id: current_user.id, board_id: params[:board_id])
-    if like.nil?
+    @like = Like.find_by(user_id: current_user.id, board_id: params[:board_id])
+    if @like.nil?
       Like.create(user_id: current_user.id, board_id: params[:board_id])
     else
-      like.destroy
+      @like.destroy
     end
-    redirect_to :back
+    @likes = Like.where(board_id: params[:board_id]).count
+    respond_to do |like_toggle|
+      like_toggle.js
+    end
   end
 end

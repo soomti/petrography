ffb0bc13a367f10b4806556cd05e7503a56cb68b
hataerin <goil1113@likelion.org>
2019-02-09 21:43:13 +0900

    이미지 크기 조정

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -7,6 +7,12 @@ class MypagesController < ApplicationController
 		@boards = Board.where(user: current_user)
 	end
 
+	def my_board_delete
+		Board.destroy(params[:board_id])
+
+		redirect_to :back
+	end
+
 	def my_like
 		
 	end
